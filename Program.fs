c43efdaa49c76da688b58d7942da2447dd972405
Jason Brown <jason@scopear.com>
2018-12-27 23:06:30 -0700

    Day 1 part 1 solution

---
+++ Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let filename = "input.txt"
+
+    let sum =
+        File.ReadLines(filename)
+        |> Seq.map Int32.Parse
+        |> Seq.sum
+
+    printfn "%d" sum
+    0 // return an integer exit code
