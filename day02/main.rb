1ae652fc7318ddf320fa0406d50e11d325517f31
Jason Brown <jasonkennethbrown@gmail.com>
2019-12-03 00:48:48 -0700

    Day 2 Part 2 solution

--- day02/main.rb
+++ day02/main.rb
@@ -3,23 +3,20 @@ require "CSV"
 module Day02
     class ProgramRunner
         def initialize(intcodes)
-            @offset = 0
+            @program_counter = 0
             @intcodes = intcodes
         end
 
         def run()
-            while (!check_for_halt(@intcodes[@offset]))
-                debug_print()
-                operation = get_operation(@intcodes[@offset])
-                execute(operation, @intcodes[@offset+1], @intcodes[@offset+2], @intcodes[@offset+3])
-                @offset += 4
+            while (!check_for_halt(@intcodes[@program_counter]))
+                operation = get_operation(@intcodes[@program_counter])
+                execute(
+                    operation,
+                    @intcodes[@program_counter+1],
+                    @intcodes[@program_counter+2],
+                    @intcodes[@program_counter+3])
+                @program_counter += 4
             end
-
-            debug_print()
-        end
-
-        def debug_print()
-            puts "pc=#{@offset}, memory=#{@intcodes.inspect}"
         end
 
         private
@@ -44,10 +41,9 @@ module Day02
         end
     end
 
-    def self.part1(intcodes)
-        # setup 1202 program alarm state
-        intcodes[1] = 12
-        intcodes[2] = 2
+    def self.run_with_inputs(intcodes, noun, verb)
+        intcodes[1] = noun
+        intcodes[2] = verb
 
         runner = ProgramRunner.new(intcodes)
         runner.run()
@@ -55,11 +51,30 @@ module Day02
         intcodes[0]
     end
 
+    def self.part1(intcodes)
+        # setup 1202 program alarm state
+        noun = 12
+        verb = 2
+        run_with_inputs(intcodes, noun, verb)
+    end
+
     def self.part2(intcodes)
-        
+        target_output = 19690720
+
+        for noun in 0..99 do
+            for verb in 0..99 do
+                output = run_with_inputs(intcodes.map(&:clone), noun, verb)
+                if output == target_output
+                    return 100 * noun + verb
+                end
+            end
+        end
+        -1
     end
 end
 
 intcodes = CSV.parse(File.read("input.txt"))[0].map { |x| x.to_i }
 puts "Part 1: " + Day02.part1(intcodes).to_s
+
+intcodes = CSV.parse(File.read("input.txt"))[0].map { |x| x.to_i }
 puts "Part 2: " + Day02.part2(intcodes).to_s
