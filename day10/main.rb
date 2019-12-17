e200a300fe37e0fa7108bea4dc5085bb44006b07
Jason Brown <jasonkennethbrown@gmail.com>
2019-12-17 01:17:36 -0700

    Day 10 Part 2 solution

--- day10/main.rb
+++ day10/main.rb
@@ -1,10 +1,10 @@
 module Day10
-    def self.parse_map(asteroid_map)
+    def self.parse_map(asteroid_map, initializer)
         asteroids = {}
         asteroid_map.each_with_index do |row, row_index|
             row.each_char.each_with_index do |cell, col_index|
                 if cell == "#"
-                    asteroids[[col_index,row_index]] = []
+                    asteroids[[col_index,row_index]] = initializer.call
                 end
             end
         end
@@ -36,8 +36,8 @@ module Day10
         !has_occluder
     end
 
-    def self.part1(asteroid_map)
-        asteroid_visibilities = parse_map(asteroid_map)
+    def self.find_best_location(asteroid_map)
+        asteroid_visibilities = parse_map(asteroid_map, lambda { [] })
 
         asteroid_visibilities.each do |coords, visible_asteroids|
             other_coords = asteroid_visibilities.keys.filter { |c| c != coords }
@@ -47,16 +47,57 @@ module Day10
             end
         end
 
-        asteroid_visibilities
-            .max_by { |coord, visible_list| visible_list.length }[1]
-            .length
+        asteroid_visibilities.max_by { |coord, visible_list| visible_list.length }
     end
 
-    def self.part2(asteroid_map)
+    def self.part1(location_and_visible_asteroids)
+        location_and_visible_asteroids[1].length
+    end
+
+    def self.angle_relative_to(origin, point)
+        atan = Math.atan2(origin[1] - point[1], origin[0] - point[0])
+
+        # adjust to "up" being the origin axis (atan2 is "right" as origin)
+        if atan < Math::PI/2
+            angle = atan + 3*Math::PI/2
+        else
+            angle = atan - Math::PI/2
+        end
+
+        angle
+    end
+
+    def self.part2(asteroid_map, monitoring_location)
+        asteroids = parse_map(asteroid_map, lambda { 0 }).keys
+
+        angle_sorted_groups = asteroids
+            .filter { |coord| coord != monitoring_location }
+            .group_by { |coord| angle_relative_to(monitoring_location, coord) }
+            .sort_by { |angle, _coll| angle }
+            .to_h
+
+        angle_sorted_groups.each { |angle, coords|
+            angle_sorted_groups[angle] = coords.sort_by {|c| distance(monitoring_location, c)}
+        }
+        
+        destructions = []
+        loop do
+            angle_sorted_groups.each do |angle, coords|
+                if coords != nil
+                    destructions << coords.shift
+                end
+            end
+
+            break if angle_sorted_groups.all? { |angle, coords| coords == nil || coords.length == 0 }
+        end
+
+        two_hundredth = destructions[199]
+        two_hundredth[0]*100 + two_hundredth[1]
     end
 end
 
 asteroid_map = File.readlines("input.txt")
 
-puts "Part 1: " + Day10.part1(asteroid_map).to_s
-puts "Part 2:\n" + Day10.part2(asteroid_map).to_s
+monitoring_station = Day10.find_best_location(asteroid_map)
+puts "Part 1: " + Day10.part1(monitoring_station).to_s
+puts "Part 2: " + Day10.part2(asteroid_map, monitoring_station[0]).to_s
