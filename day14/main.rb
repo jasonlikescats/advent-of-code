5d8efdee54495bc34b51dc94c0ff33eb088ed0a8
Jason Brown <jasonkennethbrown@gmail.com>
2020-01-03 00:18:42 -0700

    Day 14 Part 1 solution

--- day14/main.rb
+++ day14/main.rb
@@ -1,12 +1,12 @@
 require "csv"
 
 module Day14
-    class Component
-        def initialize(chemical, count)
-            @chemical, @count = chemical, count
+    class Chemical
+        def initialize(name, count)
+            @name, @count = name, count
         end
 
-        attr_reader :chemical
+        attr_reader :name
         attr_reader :count
     end
 
@@ -14,13 +14,54 @@ module Day14
         lines
             .map { |line| line.scan /[ ]*([\d]*) ([\w]+)/ }
             .map { |line_matches|
-                components = line_matches.map { |match| Component.new(match[1], match[0]) }
-                [ components[-1], components[ 0..-2 ] ]
+                chemicals = line_matches.map { |match| Chemical.new(match[1].to_sym, match[0].to_i) }
+                [ chemicals[-1], chemicals[ 0..-2 ] ]
             }
+            .to_h
+    end
+
+    class FunctionalReactor
+        def initialize reactions
+            define_reaction_methods reactions
+        end
+
+        def ore_count chemical
+            reset
+            send chemical
+            @ore_count
+        end
+
+        private
+
+        def reset
+            @ore_count = 0
+            @stockpile = Hash.new(0)
+        end
+
+        def define_reaction_methods reactions
+            reactions.each do |resultant, components|
+                FunctionalReactor.define_method(resultant.name) do
+                    if @stockpile[resultant.name] > 0
+                        @stockpile[resultant.name] -= 1
+                    else
+                        components.each do |component|
+                            component.count.times { send component.name }
+                        end
+
+                        @stockpile[resultant.name] += resultant.count - 1
+                    end
+                end
+            end
+        end
+
+        def ORE
+            @ore_count += 1
+        end
     end
 
     def self.part1(reactions)
-        
+        reactor = FunctionalReactor.new reactions
+        reactor.ore_count "FUEL"
     end
 
     def self.part2(reactions)
