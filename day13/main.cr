8149c4f8daaeedb458a7c65951f9723e63fb9f23
Jason Brown <jason@pop-os.localdomain>
2020-12-17 00:02:42 -0700

    Day 13b solution

--- day13/main.cr
+++ day13/main.cr
@@ -1,36 +1,64 @@
 require "file"
 
 class BusSchedule
-  getter number : Int32
+  getter interval : Int64, offset : Int32
 
-  def initialize(@number)
+  def initialize(@interval, @offset)
   end
 
-  def wait(ready_time)
-    since_last = ready_time % number
-    number - since_last
+  def wait(time)
+    since_last = time % interval
+    return 0 if since_last == 0
+
+    interval - since_last
+  end
+
+  def next_arrival(start_time, search_step_size)
+    start_time.step(by: search_step_size) do |time|
+      return time if wait(time + offset) == 0
+    end
   end
 end
 
 def load_input
   lines = File.read_lines("input")
   ready_time = lines[0].to_i
-  schedules = lines[1].split(',').reject!("x").map(&.to_i).map(&->BusSchedule.new(Int32))
-  {ready_time, schedules}
+  intervals = lines[1].split(',')
+  busses = Array(BusSchedule).new
+  intervals.each_with_index do |interval, offset|
+    next if interval == "x"
+
+    busses << BusSchedule.new(interval.to_i64, offset)
+  end
+  {ready_time, busses}
 end
 
 def part1
   ready_time, schedules = load_input
   schedules
-    .map { |sched| {sched.number, sched.wait(ready_time)} }
+    .map { |sched| {sched.interval, sched.wait(ready_time)} }
     .min_by { |_num, wait| wait }
     .product
 end
 
 def part2
-  data = load_input
-  
-  "TODO"
+  _, busses = load_input
+
+  time = 0_i64
+  step_size = 1_i64
+  busses.each do |bus|
+    time = bus.next_arrival(time, step_size)
+
+    # We can take bigger steps so long as they align with all our bus
+    # intervals evaluated so far. There's a danger that we overstep
+    # the increase by using multiplication here and we should evaluate
+    # the lowest common multiple instead, but luckily our input numbers
+    # have the property that lcm(x, y) == x * y (there's probably a fancy
+    # term for that).
+    step_size *= bus.interval
+  end
+
+  time
 end
 
 puts "Part 1 Result: \n#{part1}"
