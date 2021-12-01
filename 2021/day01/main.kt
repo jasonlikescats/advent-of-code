51ac53184ed45bc0c79907c7a6e55b72d3df7498
Jason Brown <jason.brown@granify.com>
2021-12-01 00:30:56 -0700

    2021 day 1 solution

---
+++ 2021/day01/main.kt
@@ -0,0 +1,33 @@
+import java.io.File
+
+fun readInput(): List<Int> {
+    return File("input").readLines().map { it.toInt() }
+}
+
+fun part1(depths: List<Int>): Int {
+    var increasingCounter = 0
+    for (i in depths.indices.drop(1)) {
+        if (depths[i] > depths[i-1]) {
+            increasingCounter++
+        }
+    }
+    return increasingCounter
+}
+
+fun part2(depths: List<Int>): Int {
+    var increasingCounter = 0
+    for (i in depths.indices.drop(3)) {
+        val prevWindowSum = depths[i-1] + depths[i-2] + depths[i-3]
+        val currWindowSum = depths[i] + depths[i-1] + depths[i-2]
+        if (currWindowSum > prevWindowSum) {
+            increasingCounter++
+        }
+    }
+    return increasingCounter
+}
+
+fun main() {
+    val depths = readInput()
+    println("Part 1 Result: \n${part1(depths)}\n")
+    println("Part 2 Result: \n${part2(depths)}")
+}
