e635c71b5fdd66a9a4006644d6e3d822392b407d
Jason Brown <jasonkennethbrown@gmail.com>
2022-12-11 23:43:05 -0700

    2022 day 4 part 2

--- 2022/day04/solution.py
+++ 2022/day04/solution.py
@@ -6,15 +6,18 @@ class Assignments:
 
     def has_full_overlap(self):
         return (
-            self.check_sections_overlap(self.section1, self.section2) or
-            self.check_sections_overlap(self.section2, self.section1)
+            self.check_sections_full_overlap(self.section1, self.section2) or
+            self.check_sections_full_overlap(self.section2, self.section1)
         )
 
-    def check_sections_overlap(self, x, y):
+    def check_sections_full_overlap(self, x, y):
         return x[0] >= y[0] and x[1] <= y[1]
 
-#section1: ('13', '93')
-#section2: ('5', '12')
+    def has_partial_overlap(self):
+        return self.check_sections_partial_overlap(self.section1, self.section2)
+
+    def check_sections_partial_overlap(self, x, y):
+        return x[0] <= y[1] and x[1] >= y[0]
 
 def part1(input):
     assignments = map(Assignments, input.split('\n'))
@@ -22,4 +25,6 @@ def part1(input):
     return len(overlapping)
     
 def part2(input):
-    return 0
+    assignments = map(Assignments, input.split('\n'))
+    overlapping = list(filter(Assignments.has_partial_overlap, assignments))
+    return len(overlapping)
