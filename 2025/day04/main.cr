be890ed820a044121fba818b92bdd17e0718ab51
Jason Brown <jasonkennethbrown@gmail.com>
2025-12-24 21:07:07 -0700

    2025 day 4 part 2

--- 2025/day04/main.cr
+++ 2025/day04/main.cr
@@ -1,34 +1,45 @@
 require "file"
 
 def load_input
-  File.read_lines("input").map(&.chars)
+  filename = "input"
+
+  {% if flag?(:sample) %}
+    filename = "sample_input"
+  {% end %}
+  
+  File.read_lines(filename).map(&.chars)
 end
 
 class Grid
   @@empty = '.'
   @@full = '@'
+  @@marked = 'x'
 
   def initialize(grid : Array(Array(Char)))
     @grid = grid
   end
 
-  def unblocked_count(max_neighbors = 4)
-    count = 0
+  def self.unblocked_count(marked_grid)
+    marked_grid.sum { |row| row.count { |cell| cell == @@marked }}
+  end
+
+  def mark_unblocked(max_neighbors = 4)
     processed_grid = @grid.clone
 
     @grid.each_with_index do |row, y|
       row.each_with_index do |cell, x|
         is_paper = cell == @@full
         if is_paper && full_neighbor_count(y, x) < max_neighbors
-          count += 1
-          processed_grid[y][x] = 'x'
+          processed_grid[y][x] = @@marked
         end
       end
     end
 
-    processed_grid.each { |row| puts row.join }
+    processed_grid
+  end
 
-    count
+  def unblock(marked_grid)
+    @grid = marked_grid
   end
 
   private def full_neighbor_count(row, col)
@@ -39,10 +50,12 @@ class Grid
 
     full_count -= 1 if local_grid[1][1] == @@full
 
-    puts "PROCESSING #{row}, #{col} - #{local_grid[1][1]}"
-    local_grid.each { |row| puts row.join }
-    puts "COUNT: #{full_count}"
-    puts ""
+    {% if flag?(:viz) %}
+      puts "PROCESSING #{row}, #{col}"
+      local_grid.each { |row| puts row.join }
+      puts "COUNT: #{full_count}"
+      puts ""
+    {% end %}
 
     full_count
   end
@@ -66,13 +79,24 @@ end
 
 def part1
   grid = Grid.new(load_input)
-  grid.unblocked_count
+  Grid.unblocked_count(grid.mark_unblocked)
 end
 
 def part2
-  data = load_input
+  grid = Grid.new(load_input)
   
-  "TODO"
+  last_count = 0
+  loop do
+    marked_grid = grid.mark_unblocked
+    count = Grid.unblocked_count(marked_grid)
+    
+    break if count == last_count
+    
+    grid.unblock(marked_grid)
+    last_count = count
+  end
+
+  last_count
 end
 
 puts "Part 1 Result: \n#{part1}"
