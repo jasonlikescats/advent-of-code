8f33e62f315c76f3b938da5b05d20455e15ed42a
Jason Brown <jasonkennethbrown@gmail.com>
2025-12-27 14:19:42 -0700

    2025 day 6 part 2

--- 2025/day06/main.cr
+++ 2025/day06/main.cr
@@ -1,15 +1,15 @@
 require "file"
 
 class Problem
-  def initialize(operand : String, args : Array(Int64))
+  def initialize(operand : Char, args : Array(Int64))
     @operand = operand
     @args = args
   end
 
   def solve : Int64
-    if @operand == "*"
+    if @operand == '*'
       @args.product
-    elsif @operand == "+"
+    elsif @operand == '+'
       @args.sum
     else
       raise "Invalid operand"
@@ -17,33 +17,64 @@ class Problem
   end
 end
 
-def load_input : Array(Problem)
+def load_input
   filename = "input"
 
   {% if flag?(:sample) %}
     filename = "sample_input"
   {% end %}
   
-  lines = File
-    .read_lines(filename)
-    .map { |line| line.strip.split(/\s+/) }
+  File.read_lines(filename)
+end
 
-  res = lines[-1].each_with_index
-  res.map do |operand, i|
+def parse_lines_row_based(lines) : Array(Problem)
+  lines = lines.map { |line| line.strip.split(/\s+/) }
+
+  operands = lines[-1].each_with_index
+  operands.map do |operand, i|
     args = lines[0...-1].map { |line| line[i].to_i64 }
-    Problem.new(operand.strip, args)
+    Problem.new(operand.strip[0], args)
   end.to_a
 end
 
+def parse_lines_columnar(lines) : Array(Problem)
+  operand_line = lines[-1]
+  problem_start_indexes = operand_line
+    .chars
+    .each_with_index
+    .select { |char, _| !char.whitespace? }
+    .map { |char, idx| idx }
+    .to_a
+  
+  problem_start_indexes << operand_line.size + 1
+
+  problems = [] of Problem
+  problem_start_indexes.each_cons_pair do |start_idx, end_idx|
+    operand = lines[-1][start_idx]
+    span = end_idx - start_idx - 1
+
+    args = [] of Int64
+    span.times do |i|
+      chars = lines[0...-1].map { |line| line[start_idx + i] }
+      args << chars.join.to_i64
+    end
+
+    problems << Problem.new(operand, args)
+  end
+
+  problems
+end
+
 def part1
-  problems = load_input
+  lines = load_input
+  problems = parse_lines_row_based(lines)
   problems.sum(&.solve)
 end
 
 def part2
-  data = load_input
-  
-  "TODO"
+  lines = load_input
+  problems = parse_lines_columnar(lines)
+  problems.sum(&.solve)
 end
 
 puts "Part 1 Result: \n#{part1}"
